31ce118389d80969c091b2633f24830a7c04c06c
Enrique Nuñez <enrique.nunyez@gmail.com>
2021-07-05 09:27:48 +0200

    finish home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { OptForm, Feature } from '../components';
 import HeaderContainer from '../containers/header';
-import { OptForm } from '../components';
 import JumbotronContainer from '../containers/jumbotron';
 import FooterContainer from '../containers/footer';
 import FaqsContainer from '../containers/faqs';
+
 
 export default function Home() {
   return (
     <>  
       <HeaderContainer>
-        <OptForm>
-          <OptForm.Input placeholder="Email address"/>
-          <OptForm.Button>Try it now</OptForm.Button>
-          <OptForm.Break></OptForm.Break>
-          <OptForm.Text>Ready to watch? Enter your email and start your netflex account now</OptForm.Text>
-        </OptForm>
+        <Feature>
+          <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+          <Feature.SubTitle>Watch any time. Cancel at any time.</Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email address"/>
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break></OptForm.Break>
+            <OptForm.Text>Ready to watch? Enter your email and start your netflex account now</OptForm.Text>
+          </OptForm>
+        </Feature>
       </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
